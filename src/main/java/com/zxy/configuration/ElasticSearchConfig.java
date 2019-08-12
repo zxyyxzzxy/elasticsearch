9e5fabfac14e56afbe265a74828b03496e0e5a70
周鑫宇 <zxyyxzzxy001@126.com>
2019-08-12 19:18:57 +0800

    中文分词查询接口

--- src/main/java/com/zxy/configuration/ElasticSearchConfig.java
+++ src/main/java/com/zxy/configuration/ElasticSearchConfig.java
@@ -18,7 +18,7 @@ public class ElasticSearchConfig {
     @Bean
     public TransportClient client() throws UnknownHostException {
         //端口为elasticsearch的TCP端口9300，不是http的9200
-        TransportAddress transportAddress = new TransportAddress(InetAddress.getByName("104.207.140.151"), 9300);
+        TransportAddress transportAddress = new TransportAddress(InetAddress.getByName("66.42.74.152"), 9300);
 
         Settings settings = Settings.builder().put("cluster.name", "ashin").build();
 
